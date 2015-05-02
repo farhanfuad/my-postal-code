db46b5df4bb02a926c226c18d9d772ab43acf0da
Ahmad Farhan <gelakketawa@gmail.com>
2015-05-02 10:59:58 +0800

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>MYPostalCode v1.0.0 Guide</title>
+</head>
+
+<body>
+Postal Code : <input type="text" name="poskod" id="my_postalcode">
+<!-- KINDLY READ ALL THE COMMENT
+
+"my_postalcode" is pre-defined attribute value for id. For Postal Code input medium such as text, list and radio.
+Attribute name is not pre-defined so you can use your own value for attribute name to make you easier to Send the data via Post or Get later.
+
+-->
+<br/>
+<br/>
+
+
+    City - <input disabled="disabled" type="text" name="bandar" id="my_city" />
+<!-- KINDLY READ ALL THE COMMENT
+
+"my_city" is pre-defined attribute value for id. For Postal Code input medium such as text, list and radio.
+Attribute name is not pre-defined so you can use your own value for attribute name to make you easier to Send the data via Post or Get later.
+
+-->
+	<br />
+    <br />
+    State- <input disabled="disabled" type="text" name="negeri" id="my_state" />
+<!-- KINDLY READ ALL THE COMMENT
+
+"my_state" is pre-defined attribute value for id. For Postal Code input medium such as text, list and radio.
+Attribute name is not pre-defined so you can use your own value for attribute name to make you easier to Send the data via Post or Get later.
+
+-->
+
+</body>
+
+<!--JQuery-->
+<script src="mypostalcode/js/mypostalcode.full.js"></script> 
+<!-- KINDLY READ ALL THE COMMENT
+
+"mypostalcode.full.js" containing MYPostalCode JavaScript code Combining Together with "JQuery-2.1.3.min.js". 
+
+IF YOU ALREADY INCLUDE JQUERY FILE KINDLY USE mypostalcode.full.js
+IF YOU NOT USING ANY JQUERY PLEASE USE mypostalcode.full.js
+
+-->
+
+</html>
